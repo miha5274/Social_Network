0802be771051b6d8c2a4f1356311d20b895f410e
mihai <mihai@NETPC>
2023-03-05 17:17:35 +0200

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var services = builder.Services;
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.Run(async context =>
+{
+    var sb = new StringBuilder();
+    sb.Append("<h1>Все сервисы</h1>");
+    sb.Append("<table>");
+    sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
+    foreach (var svc in services)
+    {
+        sb.Append("<tr>");
+        sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+        sb.Append($"<td>{svc.Lifetime}</td>");
+        sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+        sb.Append("</tr>");
+    }
+    sb.Append("</table>");
+    context.Response.ContentType = "text/html;charset=utf-8";
+    await context.Response.WriteAsync(sb.ToString());
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
